5f56c5477fb90dccc4d14c95c2c9f59472abe380
Mohd Ishrat <mohdishrat96@gmail.com>
2020-12-26 14:31:43 +0530

    Api data used

---
+++ src/Api.jsx
@@ -0,0 +1,52 @@
+const ApiData = [
+    {
+        imgName : 'https://picsum.photos/200/200?random=1',
+        textName : 'Lorum imsum text 1',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=2',
+        textName : 'Lorum imsum text 2',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=3',
+        textName : 'Lorum imsum text 3',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=4',
+        textName : 'Lorum imsum text 4',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=5',
+        textName : 'Lorum imsum text 5',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=6',
+        textName : 'Lorum imsum text 6',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=7',
+        textName : 'Lorum imsum text 7',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=8',
+        textName : 'Lorum imsum text 8',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=9',
+        textName : 'Lorum imsum text 9',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=10',
+        textName : 'Lorum imsum text 10',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=11',
+        textName : 'Lorum imsum text 11',
+    },
+    {
+        imgName : 'https://picsum.photos/200/200?random=12',
+        textName : 'Lorum imsum text 12',
+    }
+];
+
+export default ApiData;
--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import './App.css';
 import Card from './Card';
 import Footer from './Footer';
+import ApiData from './Api';
 
 function Home()
 {
@@ -10,33 +11,33 @@ function Home()
         <>
             <Navbar/>
             <Card
-                images1="https://picsum.photos/200/200?random=1"
-                text1="lorum ipsum"
-                images2="https://picsum.photos/200/200?random=2"
-                text2="lorum ipsum 2"
-                images3="https://picsum.photos/200/200?random=3"
-                text3="lorum ipsum 3"
-                images4="https://picsum.photos/200/200?random=4"
-                text4="lorum ipsum 4"
-                images5="https://picsum.photos/200/200?random=5"
-                text5="lorum ipsum 5"
-                images6="https://picsum.photos/200/200?random=6"
-                text6="lorum ipsum 6"
+                images1={ApiData[0].imgName}
+                text1={ApiData[0].textName}
+                images2={ApiData[1].imgName}
+                text2={ApiData[1].textName}
+                images3={ApiData[2].imgName}
+                text3={ApiData[2].textName}
+                images4={ApiData[3].imgName}
+                text4={ApiData[3].textName}
+                images5={ApiData[4].imgName}
+                text5={ApiData[4].textName}
+                images6={ApiData[5].imgName}
+                text6={ApiData[5].textName}
             />
 
             <Card
-                images1="https://picsum.photos/200/200?random=7"
-                text1="lorum ipsum 7"
-                images2="https://picsum.photos/200/200?random=8"
-                text2="lorum ipsum 8"
-                images3="https://picsum.photos/200/200?random=9"
-                text3="lorum ipsum 9"
-                images4="https://picsum.photos/200/200?random=10"
-                text4="lorum ipsum 10"
-                images5="https://picsum.photos/200/200?random=11"
-                text5="lorum ipsum 11"
-                images6="https://picsum.photos/200/200?random=12"
-                text6="lorum ipsum 12"
+                images1={ApiData[6].imgName}
+                text1={ApiData[6].textName}
+                images2={ApiData[7].imgName}
+                text2={ApiData[7].textName}
+                images3={ApiData[8].imgName}
+                text3={ApiData[8].textName}
+                images4={ApiData[9].imgName}
+                text4={ApiData[9].textName}
+                images5={ApiData[10].imgName}
+                text5={ApiData[10].textName}
+                images6={ApiData[11].imgName}
+                text6={ApiData[11].textName}
             />
 
             <Footer />
